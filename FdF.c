c3b01247959c39aa635d476463daa89d9b7a4328
jibot <ibotjules@gmail.com>
2022-01-05 21:32:42 +0100

    le cercle BOUGE

--- FdF.c
+++ FdF.c
@@ -6,7 +6,7 @@
 /*   By: jibot <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 13:35:32 by jibot             #+#    #+#             */
-/*   Updated: 2022/01/03 18:37:32 by jibot            ###   ########.fr       */
+/*   Updated: 2022/01/05 21:28:46 by jibot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,16 @@ typedef struct s_data {
 typedef struct s_vars {
 	void	*mlx;
 	void	*win;
+	int x_center;
+	int y_center;
+	int radius;
+	int thick;
+	int move;
 }	t_vars;
+
+int	ft_render(t_vars *vars);
+
+t_data	ft_draw_circle(t_vars *circle, t_data img);
 
 void	ft_mlx_pixput(t_data *data, int x, int y, int color)
 {
@@ -35,71 +44,75 @@ void	ft_mlx_pixput(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int	ft_close(int keycode, t_vars *vars)
+int	ft_is_circle(int x, int y, t_vars *circle)
 {
-	if (keycode == 53)
-	mlx_destroy_window(vars->mlx, vars->win);
+	int sqr_sum;
+
+	sqr_sum = ft_pwr(x - circle->x_center, 2) + ft_pwr(y - circle->y_center, 2);
+	if (sqr_sum > ft_pwr(circle->radius, 2) - circle->thick \
+		&& sqr_sum < ft_pwr(circle->radius, 2) + circle->thick)
+		return (1);
+	return (0);
+}
+
+int	ft_key_handle(int keycode, t_vars *vars)
+{
+	if (keycode == 0)
+		vars->move = 1;
+	else if (keycode == 2)
+   		vars->move = -1;
+	else if (keycode == 53)
+		mlx_destroy_window(vars->mlx, vars->win);
 	return (keycode);
 }
 
-int	ft_move(t_data img, int keycode, t_vars *vars)
+t_data	ft_draw_circle(t_vars *circle, t_data img)
 {
 	int x;
 	int y;
 
+	circle->x_center += circle->move;
 	x = 0;
 	while (x < 1000)
 	{
 		y = 0;
 		while (y < 1000)
 		{
-			if (*(unsigned int*)(img.addr + (y * img.l_len + x * (img.bpp / 8))) == 0x00F0F000/*[0, 240, 240, 0]*/)
-			{
-				if (keycode == 0)
-				{
-					ft_mlx_pixput(&img, x - 1, y, 0x00F0F000);
-					ft_mlx_pixput(&img, x, y, 0x00000000);
-				}
-				else if (keycode == 2)
-				{
-					ft_mlx_pixput(&img, x + 1, y, 0x00F0F000);
-					ft_mlx_pixput(&img, x, y, 0x00000000);
-				}
-			}
-			y++;
-		}
-		x++;
-	}
-	mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
-	return (keycode);
-}
-
-int	main(void)
-{
-	int		x;
-	int		y;
-	t_vars	vars;
-	t_data	img;
-
-	x = 0;
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1000, 1000, "testwin");
-	img.img = mlx_new_image(vars.mlx, 1000, 1000);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.l_len, &img.endian);
-	while (x < 1000)
-	{
-		y = 0;
-		while (y < 1000)
-		{
-			if (ft_pwr((x - 500), 2) + ft_pwr((y - 500), 2) > ft_pwr(200, 2) - 500
-				&& ft_pwr((x - 500), 2) + ft_pwr((y - 500), 2) < ft_pwr(200, 2) + 500)
+			if (ft_is_circle(x, y, circle))
 				ft_mlx_pixput(&img, x, y, 0x00F0F000);
 			y++;
 		}
 		x++;
 	}
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	//mlx_key_hook(vars.win, ft_close, &vars);
-	mlx_key_hook(vars.win, ft_move, &vars);
+	return (img);
+}
+
+int	ft_render(t_vars *vars)
+{
+	t_data	img;
+
+	img.img = mlx_new_image(vars->mlx, 1000, 1000);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.l_len, &img.endian);
+	ft_draw_circle(vars, img);
+	mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	(void) argc;
+	static t_vars	vars;
+	t_data	img;
+
+	vars.x_center = ft_atoi(argv[1]);
+	vars.y_center = ft_atoi(argv[2]);
+	vars.radius = ft_atoi(argv[3]);
+	vars.thick = ft_atoi(argv[4]);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1000, 1000, "testwin");
+	img.img = mlx_new_image(vars.mlx, 1000, 1000);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.l_len, &img.endian);
+	mlx_hook(vars.win, 2, 0, ft_key_handle, &vars);
+	mlx_loop_hook(vars.mlx, ft_render, &vars);
 	mlx_loop(vars.mlx);
 }
